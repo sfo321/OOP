e60a47c7e3e5cef398fca7ed9b82dc92bd4e06d6
Stefan Fotev <sfo321@mail.bg>
2016-06-22 11:59:40 +0300

    fixings

---
+++ ExtensionMethods/DivisibleBy7and3/DivisibleBy7and3/Divisibvle.cs
@@ -0,0 +1,20 @@
+﻿namespace DivisibleBy7and3
+{
+    using System;
+    using System.Linq;
+
+    public class Divisibvle
+    {
+        static void Main()
+        {
+            var numbers = new[] { 5, 34, 140, 70, 21, 435, 562, 908, 900, 42, 84 };
+
+            var result = numbers.Where(x => x % 3 == 0 && x % 7 == 0).ToArray();
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/LongestString/LongestString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LongestString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var stringlist = new List<string> { "vafla", "zmiq", "kokoshka",
+                "portokal", "muu", "rabota", "kot takoa", "C reshetka", "Javascriptaaaa i NODE.js" };
+
+            var ordered = stringlist.OrderBy(x => x.Length).ToList();
+
+            foreach(string str in ordered)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
