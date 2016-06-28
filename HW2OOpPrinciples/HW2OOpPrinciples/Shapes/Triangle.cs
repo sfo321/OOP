d6ada0b9f1362ae5be46ea1f0fff8f0ac1bdcecb
Stefan Fotev <sfo321@mail.bg>
2016-06-28 22:49:24 +0300

    done

---
+++ HW2OOpPrinciples/HW2OOpPrinciples/Shapes/Triangle.cs
@@ -0,0 +1,17 @@
+﻿namespace Shapes
+{
+    using System;
+
+    public class Triangle : Shape
+    {
+        public Triangle(int width, int height) : base(width, height)
+        {
+        }
+
+        public override void CalculateSurface()
+        {
+            double result = (Width * Height) / 2;
+            Console.WriteLine("The Area of {0} is {1}", this.GetType().Name, result);
+        }
+    }
+}
