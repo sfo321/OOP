8d85a6995266ad9e824dc006b15e0af24461eae4
Stefan Fotev <sfo321@mail.bg>
2016-06-30 20:39:43 +0300

    go

---
+++ BitArray/BitArray/Array.cs
@@ -0,0 +1,105 @@
+﻿namespace BitArray
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class BitArray64 : IEnumerable<int>
+    {
+        private const int Length = 64;
+        private int[] bits = new int[Length];
+
+        public BitArray64(ulong number)
+        {
+            string binary = ConvertDecimalToBinary(number);
+            this.bits = BitsIntoArray(binary);
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > Length - 1)
+                {
+                    throw new ArgumentOutOfRangeException("Index is in range [0 - 63]");
+                }
+
+                return this.bits[index];
+            }
+        }
+
+        public static bool operator ==(BitArray64 first, BitArray64 second)
+        {
+            return (first.Equals(second));
+        }
+
+        public static bool operator !=(BitArray64 first, BitArray64 second)
+        {
+            return !(first.Equals(second));
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherArray = obj as BitArray64;
+
+            for (int i = 0; i < Length; i++)
+            {
+                if (!otherArray[i].Equals(this[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.bits.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(string.Empty, this.bits);
+        }
+
+        private string ConvertDecimalToBinary(ulong number)
+        {
+            ulong systemBase = 2;
+            string binaryRep = String.Empty;
+
+            while (number > 0)
+            {
+                ulong remainder = number % systemBase;
+                number /= systemBase;
+                binaryRep = remainder.ToString() + binaryRep;
+            }
+
+            return binaryRep.ToString().PadLeft(64, '0');
+        }
+
+        private int[] BitsIntoArray(string binary)
+        {
+            int[] bitArray = new int[64];
+            for (int i = 0; i < binary.Length; i++)
+            {
+                bitArray[i] = int.Parse(binary[i].ToString());
+            }
+
+            return bitArray;
+        }
+    }
+}
---
+++ BitArray/BitArray/Start.cs
@@ -0,0 +1,34 @@
+﻿namespace BitArray
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            var firstArray = new BitArray64(65478456456789);
+            Console.WriteLine("Binary representation: " + "\n" + firstArray.ToString());
+            Console.WriteLine("First bit = {0}", firstArray[0]);
+            Console.WriteLine("Last bit = {0}", firstArray[63]);
+
+            Console.WriteLine(firstArray[0] == firstArray[63]);
+
+
+            foreach (var bit in firstArray)
+            {
+                Console.Write(bit + " ");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Testing equality");
+
+            var secondArray = new BitArray64(65478456456789);
+            var thirdArrray = new BitArray64(65478456456788);
+
+            Console.WriteLine(firstArray.Equals(secondArray) == true ? "arrays have equal elements" : "arrays do not have equal elements");
+            Console.WriteLine(firstArray.Equals(thirdArrray) == true ? "arrays have equal elements" : "arrays do not have equal elements");
+        }
+    }
+}
