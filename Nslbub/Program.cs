5387e21ad04a4a2485d3837e1676e7871e2da82c
Stefan Fotev <sfo321@mail.bg>
2016-07-06 16:36:32 +0300

    Hah

---
+++ Nslbub/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+
+namespace Nslbub
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            var result = new StringBuilder();
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                result.Append((char)(input[i] - 1));
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
