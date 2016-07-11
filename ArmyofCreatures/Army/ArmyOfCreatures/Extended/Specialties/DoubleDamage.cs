0d662dd18fb5e398e67d6dbd5d9197bc89d710a5
Stefan Fotev <sfo321@mail.bg>
2016-07-11 13:23:26 +0300

    Done

---
+++ ArmyofCreatures/Army/ArmyOfCreatures/Extended/Specialties/DoubleDamage.cs
@@ -0,0 +1,56 @@
+﻿namespace ArmyOfCreatures.Extended.Specialties
+{
+    using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Logic.Specialties;
+    using System;
+    using System.Globalization;
+
+    public class DoubleDamage : Specialty
+    {
+        private int rounds;
+
+        public DoubleDamage(int rounds)
+        {
+            if (rounds <= 0)
+            {
+                throw new ArgumentOutOfRangeException("rounds", "The number of rounds should be greater than 0");
+            }
+
+            if (rounds > 10)
+            {
+                throw new ArgumentOutOfRangeException("rounds", "The number of rounds should be less than or equal to 10");
+            }
+
+            this.rounds = rounds;
+        }
+
+        public override decimal ChangeDamageWhenAttacking(
+            ICreaturesInBattle attackerWithSpecialty,
+            ICreaturesInBattle defender,
+            decimal currentDamage)
+        {
+            if (attackerWithSpecialty == null)
+            {
+                throw new ArgumentNullException("attackerWithSpecialty");
+            }
+
+            if (defender == null)
+            {
+                throw new ArgumentNullException("defender");
+            }
+
+            if (this.rounds <= 0)
+            {
+                return currentDamage;
+            }
+
+            this.rounds--;
+            return currentDamage * 2;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0}({1})", base.ToString(), this.rounds);
+        }
+    }
+}
--- ArmyofCreatures/Army/ArmyOfCreatures/Properties/AssemblyInfo.cs
+++ ArmyofCreatures/Army/ArmyOfCreatures/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("ArmyOfCreatures.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("ArmyOfCreatures.Console")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2be53ccc-95ab-4390-8a6d-84f3a72e7a58")]
+[assembly: Guid("8d68b1aa-c564-4a1a-812c-5ba491cfb1de")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
