3846bf1947f698baea6bfcfa84cf51f0f5162bfe
Stefan Fotev <sfo321@mail.bg>
2016-07-13 14:45:51 +0300

    done

---
+++ probaaa/Kurec.cs
@@ -0,0 +1,10 @@
+﻿namespace probaaa
+{
+    public enum Kurec
+    {
+        Tashak = 2,
+        Muflon = 4,
+        Kopele = 6,
+        Zumbaio = 8
+    }
+}
---
+++ probaaa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probaaa
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string k = Enum.GetName(typeof(Kurec), 4);
+
+            int g = (int)Kurec.Kopele;
+
+            Console.WriteLine(k);
+
+            var role = "Tashak";
+
+            Kurec t = (Kurec)Enum.Parse(typeof(Kurec), role);
+        }
+    }
+}
