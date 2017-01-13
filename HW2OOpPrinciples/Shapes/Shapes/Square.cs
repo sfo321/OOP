7336b28b375b2f6948b8315a59edbbc8e25e7155
Stefan Fotev <sfo321@mail.bg>
2017-01-13 18:59:34 +0200

    renamed

---
+++ HW2OOpPrinciples/Shapes/Shapes/Square.cs
@@ -0,0 +1,17 @@
+﻿namespace Shapes
+{
+    using System;
+
+    public class Square : Shape
+    {
+        public Square(int width) : base(width, width)
+        {
+        }
+
+        public override void CalculateSurface()
+        {
+            double result = (Width * Width);
+            Console.WriteLine("The Area of {0} is {1}", this.GetType().Name, result);
+        }
+    }
+}
